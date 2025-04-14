dcd4d6f7b801848c91d8c96860e989b8d813133c
wasabina67 <wasabina67@gmail.com>
2025-04-14 20:25:26 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
+from pdf2image import convert_from_path
+
+
+def pdf_to_images(path, dpi=300):
+    return convert_from_path(path, dpi=dpi)
+
+
 def main():
-    pass
+    sample1_pdf_path = "sample1.pdf"
+    sample2_pdf_path = "sample2.pdf"
+
+    sample1_images = pdf_to_images(sample1_pdf_path)
+    sample2_images = pdf_to_images(sample2_pdf_path)
+
+    if len(sample1_images) != len(sample2_images):
+        return
 
 
 if __name__ == "__main__":
